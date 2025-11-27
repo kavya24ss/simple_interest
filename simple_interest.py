0d94150e5609bc7777f50c7f00258b77c64fee1d
kavya24ss <sagikavyasri@gmail.com>
2025-11-27 14:06:25 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,14 +1,14 @@
 import sys
 if len(sys.argv) == 4:
     script_name=sys.argv[0]
-    principal=sys.argv[1]
-    rate=sys.argv[2]
-    time=sys.argv[3]
+    principal=float(sys.argv[1])
+    rate=float(sys.argv[2])
+    time=float(sys.argv[3])
 else:
     principal=1000.0
     rate=5.0
     time=10
-simple_interest=(principal * rate * time)/100
+simple_interest=(principal * rate * time) / 100
 print("principal Amount is",principal)
 print("rate of interest is",rate)
 print("time period is",time)
