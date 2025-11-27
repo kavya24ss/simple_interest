a9ecec23e219f8bee585e9a2b7ca023b28a58b27
kavya24ss <sagikavyasri@gmail.com>
2025-11-27 13:57:30 +0530

    Create simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,15 @@
+import sys
+if len(sys.argv) == 4:
+    script_name=sys.argv[0]
+    principal=sys.argv[1]
+    rate=sys.argv[2]
+    time=sys.argv[3]
+else:
+    principal=1000.0
+    rate=5.0
+    time=10
+simple_interest=(principal * rate * time)100
+print("principal Amount is",principal)
+print("rate of interest is",rate)
+print("time period is",time)
+print("simple interest is",simple_interst)
